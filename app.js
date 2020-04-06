f24f877f55f59f2e3a72212debd89f646532b51e
Sudhanshu Raheja <sudhanshu@leftshift.io>
2020-04-06 23:36:40 +0700

    [sudhanshu] added a healthcheck api

--- app.js
+++ app.js
@@ -1,57 +1,57 @@
-
 /**
  * Module dependencies.
  */
 
-var express = require('express');
-var routes = require('./routes');
-var http = require('http');
-var path = require('path');
+var express = require("express");
+var routes = require("./routes");
+var http = require("http");
+var path = require("path");
 
 var app = express();
 
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var methodOverride = require('method-override');
-var bodyParser = require('body-parser');
-var compression = require('compression');
-var errorHandler = require('errorhandler');
+var favicon = require("serve-favicon");
+var logger = require("morgan");
+var methodOverride = require("method-override");
+var bodyParser = require("body-parser");
+var compression = require("compression");
+var errorHandler = require("errorhandler");
 
-var projects = require('./projects').get();
+var projects = require("./projects").get();
 
 // all environments
-app.set('port', process.env.PORT || 3000);
-app.set('views', __dirname + '/views');
-app.set('view engine', 'pug');
-app.use(favicon(path.join(__dirname, '/public/images/favicon.ico')))
-app.use(logger('dev'))
-app.use(methodOverride())
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: true }))
-app.use(express.static('public'))
+app.set("port", process.env.PORT || 3000);
+app.set("views", __dirname + "/views");
+app.set("view engine", "pug");
+app.use(favicon(path.join(__dirname, "/public/images/favicon.ico")));
+app.use(logger("dev"));
+app.use(methodOverride());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
 app.use(compression());
 
 // development only
-if ('development' == app.get('env')) {
-  app.use(errorHandler())
+if ("development" == app.get("env")) {
+  app.use(errorHandler());
 }
 
-app.get('/', routes.index);
-app.get('/love', routes.love);
-app.get('/sitemap.xml', routes.sitemap);
+app.get("/healthcheck", routes.healthcheck);
+app.get("/", routes.index);
+app.get("/love", routes.love);
+app.get("/sitemap.xml", routes.sitemap);
 
-for(x in projects) {
-	app.get(projects[x].url, routes.projectCaseStudy);
+for (x in projects) {
+  app.get(projects[x].url, routes.projectCaseStudy);
 }
 
-app.get('/blog', routes.blog);
-app.get('/blog/', routes.blog);
-app.get('/blog/:page', routes.blog);
-app.get('/tag/:tag', routes.tags);
-app.get('/tag/:tag/', routes.tags);
-app.get('/tag/:tag/:page', routes.tags);
-app.get('/:post', routes.post);
+app.get("/blog", routes.blog);
+app.get("/blog/", routes.blog);
+app.get("/blog/:page", routes.blog);
+app.get("/tag/:tag", routes.tags);
+app.get("/tag/:tag/", routes.tags);
+app.get("/tag/:tag/:page", routes.tags);
+app.get("/:post", routes.post);
 
-http.createServer(app).listen(app.get('port'), function(){
-  console.log('Express server listening on port ' + app.get('port'));
+http.createServer(app).listen(app.get("port"), function () {
+  console.log("Express server listening on port " + app.get("port"));
 });
--- routes/index.js
+++ routes/index.js
@@ -1,146 +1,152 @@
-var posts = require('../blog');
-var markdown = require( "markdown" ).markdown;
-var fs = require('fs');
-var _ = require('underscore');
+var posts = require("../blog");
+var markdown = require("markdown").markdown;
+var fs = require("fs");
+var _ = require("underscore");
 
-var clients = require('../clients');
-var projects = require('../projects').get();
-var team = require('../team').get();
+var clients = require("../clients");
+var projects = require("../projects").get();
+var team = require("../team").get();
 
-exports.index = function(req, res){
-    res.render('index',
-        {
-            title: 'LeftShift | we create loveable apps for ios, android and the web',
-            description: 'Leftshift helps clients create loveable mobile applications for iOS, Android and the web',
-            clients: clients.get(),
-            projects: projects,
-            team: team
-        }
-    );
+exports.healthcheck = function (req, res) {
+  const healthCheckResponse = {
+    status: "UP",
+  };
+  res.status(200).json(healthCheckResponse);
 };
 
-exports.sitemap = function(req, res) {
-    res.render('sitemap');
+exports.index = function (req, res) {
+  res.render("index", {
+    title: "LeftShift | we create loveable apps for ios, android and the web",
+    description:
+      "Leftshift helps clients create loveable mobile applications for iOS, Android and the web",
+    clients: clients.get(),
+    projects: projects,
+    team: team,
+  });
 };
 
-exports.blog = function(req, res) {
-    var page = req.params.page;
-    if( _.isUndefined(page) ) page = 1;
-    if( isNaN(page) ) res.redirect('/blog');
-    if( page < 0 ) res.redirect('/blog');
-
-    var articlesPerPage = 8;
-    var startPostNumber = (page - 1) * articlesPerPage;
-
-    var previous = '';
-    var next = '';
-
-    if(page > 0) next = Number(page) + 1;
-    if(page > 1) previous = Number(page) - 1;
-
-    var tempPosts = posts.get().slice();
-    var list = tempPosts.splice(startPostNumber, articlesPerPage);
-
-    if(list.length < articlesPerPage) next = '';
-
-    if( _.isEmpty(list) ) {
-        res.render('404');
-    } else {
-        res.render('bloghome', {
-            posts: list,
-            title: 'Home | Page ' + page,
-            description: 'The leftshift blog',
-            next: next,
-            previous: previous
-        });
-    }
+exports.sitemap = function (req, res) {
+  res.render("sitemap");
 };
 
-exports.post = function(req, res) {
-    var tempPosts = posts.get().slice();
-    var post = _.find(tempPosts, function(selected) {
-        return selected.slug === req.params.post;
+exports.blog = function (req, res) {
+  var page = req.params.page;
+  if (_.isUndefined(page)) page = 1;
+  if (isNaN(page)) res.redirect("/blog");
+  if (page < 0) res.redirect("/blog");
+
+  var articlesPerPage = 8;
+  var startPostNumber = (page - 1) * articlesPerPage;
+
+  var previous = "";
+  var next = "";
+
+  if (page > 0) next = Number(page) + 1;
+  if (page > 1) previous = Number(page) - 1;
+
+  var tempPosts = posts.get().slice();
+  var list = tempPosts.splice(startPostNumber, articlesPerPage);
+
+  if (list.length < articlesPerPage) next = "";
+
+  if (_.isEmpty(list)) {
+    res.render("404");
+  } else {
+    res.render("bloghome", {
+      posts: list,
+      title: "Home | Page " + page,
+      description: "The leftshift blog",
+      next: next,
+      previous: previous,
     });
+  }
+};
 
-    if( !_.isEmpty(post) ) {
-        fs.readFile(__dirname + '/../posts/' + post.file, 'utf8', function(err, data) {
-            if(err) console.log(err);
-            else {
-                res.render(post.render,
-                    {
-                        title: post.title,
-                        description: post.description,
-                        author: post.author,
-                        date: post.date,
-                        data: post.prepend + markdown.toHTML(data) + post.append,
-                        tags: post.tags
-                    }
-                );
-            }
+exports.post = function (req, res) {
+  var tempPosts = posts.get().slice();
+  var post = _.find(tempPosts, function (selected) {
+    return selected.slug === req.params.post;
+  });
+
+  if (!_.isEmpty(post)) {
+    fs.readFile(__dirname + "/../posts/" + post.file, "utf8", function (
+      err,
+      data
+    ) {
+      if (err) console.log(err);
+      else {
+        res.render(post.render, {
+          title: post.title,
+          description: post.description,
+          author: post.author,
+          date: post.date,
+          data: post.prepend + markdown.toHTML(data) + post.append,
+          tags: post.tags,
         });
-    } else {
-        res.render('404');
-    }
-
-}
-
-exports.tags = function(req, res) {
-    var tag = req.params.tag;
-
-    var page = req.params.page;
-    if( _.isUndefined(page) ) page = 1;
-    if( isNaN(page) ) res.redirect('/blog');
-    if( page < 0 ) res.redirect('/blog');
-
-    var articlesPerPage = 4;
-    var startPostNumber = (page - 1) * articlesPerPage;
-
-    var previous = '';
-    var next = '';
-
-    if(page > 0) next = Number(page) + 1;
-    if(page > 1) previous = Number(page) - 1;
-
-    var tempPosts = posts.get().slice();
-    tempPosts = _.filter(tempPosts, function(post) {
-        var indexOf = _.indexOf(post.tags, tag);
-        return (indexOf >= 0);
+      }
     });
-    var list = tempPosts.splice(startPostNumber, articlesPerPage);
+  } else {
+    res.render("404");
+  }
+};
 
-    if(list.length < articlesPerPage) next = '';
+exports.tags = function (req, res) {
+  var tag = req.params.tag;
 
-    if( _.isEmpty(list) ) {
-        res.render('404');
-    } else {
-        res.render('taghome', {
-            tag: tag,
-            posts: list,
-            title: 'Home | Page ' + page,
-            description: 'The leftshift blog',
-            next: next,
-            previous: previous
-        });
-    }
-}
+  var page = req.params.page;
+  if (_.isUndefined(page)) page = 1;
+  if (isNaN(page)) res.redirect("/blog");
+  if (page < 0) res.redirect("/blog");
 
-exports.love = function(req, res){
-    res.render('love',
-        {
-            title: 'What has love got to do with making apps? | LeftShift | we create loveable apps for ios, android and the web',
-            description: 'There is only one way to make amazing software. Love is what turns a good app into a piece of art.'
-        }
-    );
+  var articlesPerPage = 4;
+  var startPostNumber = (page - 1) * articlesPerPage;
+
+  var previous = "";
+  var next = "";
+
+  if (page > 0) next = Number(page) + 1;
+  if (page > 1) previous = Number(page) - 1;
+
+  var tempPosts = posts.get().slice();
+  tempPosts = _.filter(tempPosts, function (post) {
+    var indexOf = _.indexOf(post.tags, tag);
+    return indexOf >= 0;
+  });
+  var list = tempPosts.splice(startPostNumber, articlesPerPage);
+
+  if (list.length < articlesPerPage) next = "";
+
+  if (_.isEmpty(list)) {
+    res.render("404");
+  } else {
+    res.render("taghome", {
+      tag: tag,
+      posts: list,
+      title: "Home | Page " + page,
+      description: "The leftshift blog",
+      next: next,
+      previous: previous,
+    });
+  }
+};
+
+exports.love = function (req, res) {
+  res.render("love", {
+    title:
+      "What has love got to do with making apps? | LeftShift | we create loveable apps for ios, android and the web",
+    description:
+      "There is only one way to make amazing software. Love is what turns a good app into a piece of art.",
+  });
 };
 
 // Create pages for each projects
-exports.projectCaseStudy = function(req, res) {
-    var thisProject = {};
-    for(y in projects) {
-        // We don't have trailing backslash, I'm just too lazy
-        if(projects[y].url == req.url) {
-            thisProject = projects[y];
-        }
+exports.projectCaseStudy = function (req, res) {
+  var thisProject = {};
+  for (y in projects) {
+    // We don't have trailing backslash, I'm just too lazy
+    if (projects[y].url == req.url) {
+      thisProject = projects[y];
     }
-    res.render('project', thisProject);
+  }
+  res.render("project", thisProject);
 };
